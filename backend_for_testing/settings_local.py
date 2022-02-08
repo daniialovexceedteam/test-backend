f022fe8ffa0367152c18da91e679a3ab5b52c47b
solo <dsogomonian@gmail.com>
2022-02-08 13:35:11 +0300

    refactored .gitignore, local_settings. added README.  compose file fix

--- backend_for_testing/settings_local.py
+++ backend_for_testing/settings_local.py
@@ -1,13 +1,5 @@
 from .settings import *
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = env.get("SECRET_KEY")
-
-
-ALLOWED_HOSTS = env.get("ALLOWED_HOSTS").split(" ")
-
 LOCAL_APPS = [
     'testData',
 ]
@@ -21,15 +13,22 @@ THIRD_PARTY_APPS = [
 
 INSTALLED_APPS = INSTALLED_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+NAME = env.get('POSTGRES_DB')
+USER = env.get('POSTGRES_USER')
+PASSWORD = env.get('POSTGRES_PASSWORD')
+HOST = env.get('POSTGRES_HOST')
+SECRET_KEY = env.get('SECRET_KEY')
+ALLOWED_HOSTS = env.get('ALLOWED_HOSTS', ['localhost'])
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(' ')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.get('POSTGRES_NAME'),
-        'USER': env.get('POSTGRES_USER'),
-        'PASSWORD': env.get('POSTGRES_PASSWORD'),
-        'HOST': env.get('POSTGRES_HOST')
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST
     }
 }
